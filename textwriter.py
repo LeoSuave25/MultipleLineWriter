28cec7596715801bf0051cb2683e1c06c3633a09
LeoSuave25 <suaverdez90@gmail.com>
2023-04-27 16:03:24 +0800

    Styling Done

--- textwriter.py
+++ textwriter.py
@@ -1,4 +1,9 @@
 # Style the Entire Program
+import pyfiglet as fig
+print("\033[32m","="*100,"\033[m")
+title = "Multiple Line Writer"
+print("\033[32m",fig.figlet_format(title),"\033[m")
+print(fig.figlet_format("Made by: Leoj M Suaverdez",font="bubble"))
 # Make a function to write to a file
 def writer(to_write):
     with open('myfile.txt', 'a') as outputfile:
@@ -7,14 +12,15 @@ def writer(to_write):
 # Put it in a while loop with a condition if response is 'y'
 response = 'y'
 while response.lower() == 'y':
-    print("Enter line")
+    print("\033[1;32mAre there more lines? (y/n)\033[0m")
     words_to_write = input()
     writer(words_to_write)
     while True:
-        print("Are there more lines? (y/n)")
+        print("\033[1;32mAre there more lines? (y/n)\033[0m")
         response = input()
         if response.lower() in ('y', 'n'):
             break
         else:
-            print("Invalid Response. Please Enter y or n ")
-        
+            print("\033[1;31mInvalid Response. Please Enter y or n.\033[0m")
+
+print("\033[32m","="*100,"\033[m")
