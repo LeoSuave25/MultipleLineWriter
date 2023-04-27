69c27192d19f47480e7bbc5524c1ca5c093074e1
LeoSuave25 <suaverdez90@gmail.com>
2023-04-27 15:41:20 +0800

    Function and Asking User Input Done

--- textwriter.py
+++ textwriter.py
@@ -1,4 +1,10 @@
 # Style the Entire Program
 # Make a function to write to a file
+def writer(to_write):
+    with open('myfile.txt', 'a') as outputfile:
+        outputfile.write(str(to_write) + "\n")
 # Ask for user input what to write in the myfile.txt
+print("Enter line")
+words_to_write = input()
+writer(words_to_write)
 # Put it in a while loop with a condition if response is 'y'
