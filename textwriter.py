f334ede87edf127b646c2a09798c9a964db99487
LeoSuave25 <suaverdez90@gmail.com>
2023-04-27 15:53:05 +0800

    While Loop Done

--- textwriter.py
+++ textwriter.py
@@ -4,7 +4,17 @@ def writer(to_write):
     with open('myfile.txt', 'a') as outputfile:
         outputfile.write(str(to_write) + "\n")
 # Ask for user input what to write in the myfile.txt
-print("Enter line")
-words_to_write = input()
-writer(words_to_write)
 # Put it in a while loop with a condition if response is 'y'
+response = 'y'
+while response.lower() == 'y':
+    print("Enter line")
+    words_to_write = input()
+    writer(words_to_write)
+    while True:
+        print("Are there more lines? (y/n)")
+        response = input()
+        if response.lower() in ('y', 'n'):
+            break
+        else:
+            print("Invalid Response. Please Enter y or n ")
+        
